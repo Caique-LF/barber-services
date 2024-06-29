a72fd4c903869e01ff3c8eb562768b33ca1838e5
Caique L. Fernandes <caique.l.f9.clf@gmail.com>
2024-06-29 18:16:20 -0400

    create module experts and create experts endpoint

---
+++ src/experts/experts.controller.ts
@@ -0,0 +1,30 @@
+import {
+	BadRequestException,
+	Body,
+	Controller,
+	HttpStatus,
+	Post,
+	Res
+} from '@nestjs/common'
+import { ExpertsService } from './experts.service'
+import CreateExpertsDto from './dtos/create-experts'
+import { Response } from 'express'
+
+@Controller('experts')
+export class ExpertsController {
+	constructor(private readonly expertsService: ExpertsService) {}
+
+	@Post()
+	async create(@Body() data: CreateExpertsDto, @Res() res: Response) {
+		const expertExist = await this.expertsService.findExpertByEmail(data.email)
+
+		if (expertExist) {
+			throw new BadRequestException(
+				'Já existe um profissional com o email informado'
+			)
+		}
+
+		const expert = await this.expertsService.createExpert(data)
+		return res.status(HttpStatus.CREATED).json(expert)
+	}
+}
