b3b206096bd28ba320ee558574d5b7e14ec65178
caique <caique.l.f9.clf@gmail.com>
2024-07-09 20:19:12 -0400

    config dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 async function bootstrap() {
+	console.log('JWT_SECRET:', process.env.JWT_SECRET)
 	const app = await NestFactory.create(AppModule)
 	app.useGlobalPipes(new ValidationPipe())
 	await app.listen(3000)
